4783c37870e3f2963c7eac9a38a1fc85d68863ae
Ukkasha Farqaleet <154653274+ukkashafarqaleet@users.noreply.github.com>
2024-09-15 14:38:06 +0530

    Update settings.py

--- parenting/settings.py
+++ parenting/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-9wcl543^yzva!_dx#%f-3q36no05#5uk_%g*&@z3ruz)k*c04='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
